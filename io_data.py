7b62d83a0242b6e5017d9d78aeb40581e5085774
Aleks <LyohaVovk@gmail.com>
2024-02-26 22:58:55 +0300

    добавление возврата команд

--- io_data.py
+++ io_data.py
@@ -2,12 +2,20 @@
 
 def input_notes():
     print("Здесь вводим заметку")
+    command = '0'
+    return command
 
 def output_notes():
     print("Здесь выводим существующую заметку")
+    command = '0'
+    return command
 
 def edit_notes():
     print("Здесь редактируем заметку")
+    command = '0'
+    return command
 
 def del_notes():
     print("Здесь удаляем заметку")
+    command = '0'
+    return command
