d2a5128c28efcccdcfb044fde25859876c750dd9
Aleks <LyohaVovk@gmail.com>
2024-02-26 22:55:54 +0300

    Обработка суперцикла

--- ui.py
+++ ui.py
@@ -5,20 +5,24 @@ from io_data import input_notes, output_notes, edit_notes, del_notes
 
 def main_screen():
     clear_screen()
-    command = menu()
+    menu()
+    command = input("Введите команду => ")
     while command != '5':
         
         while command != '1' and command != '2' and command != '3' and command != '4':
             print("Введена неподдерживаемая команда! \n Попробуйте еще раз.")
-            command = input("Введите команду: ")
-        
-        if command == '1':
-            input_notes()
+            command = input("Введите команду => ")
+            
+        if command == '0':
+            command = input("Введите команду => ")
+            
+        elif command == '1':
+            command = input_notes()
         elif command == '2':
-            edit_notes()
+            command = edit_notes()
         elif command == '3':
-            output_notes()
+            command = output_notes()
         elif command == '4':
-            del_notes()
+            command = del_notes()
         elif command == '5':
             exit(0)               
