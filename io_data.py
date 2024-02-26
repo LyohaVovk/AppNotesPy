c4885bffeb92bdd020a42efc85bb8505fec64531
Aleks <LyohaVovk@gmail.com>
2024-02-26 21:00:10 +0300

    Созданы заготовки для функций

---
+++ io_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def input_notes():
+    pass
+
+def output_notes():
+    pass
+
+def edit_notes():
+    pass
+
+def del_notes():
+    pass
--- ui.py
+++ ui.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+from io_data import input_notes, output_notes, edit_notes, del_notes
 
 def interface():
-    pass
+    print("Добро пожаловать в AppNotes")
+    print("1. - Создать заметку;\n2. - Редактировать заметку;\n3. - Вывод заметок;\n4. - Удаление заметок;\n5. - Выход из программы")
+    command = int(input("Введите команду: "))
+    while command != 1 and command != 2 and command != 3 and command != 4 and command != 5:
+        print("Введена неподдерживаемая команда! \n Попробуйте еще раз.")
+        command = int(input("Введите команду: "))
+     
+    if command == 1:
+        input_notes()
+    elif command == 2:
+        edit_notes()
+    elif command == 3:
+        output_notes()
+    elif command == 4:
+        del_notes()
+    elif command == 5:
+        pass
