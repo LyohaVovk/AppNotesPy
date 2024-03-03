8c976c42dbb44942e79ed51889934efaf45c9e76
Aleks <LyohaVovk@gmail.com>
2024-03-03 22:49:23 +0300

    передела модель *.json файла для удобства отображения и записи

--- io_data.py
+++ io_data.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+import json
+
 
 def input_notes():
-    print("Здесь вводим заметку")
+    # print("Здесь вводим заметку")
+    
     command = '0'
     return command
 
 def output_notes():
-    print("Здесь выводим существующую заметку")
+    # print("Здесь выводим существующую заметку")
+    with open('Notes.json', 'r', encoding='utf-8') as f:
+        notes_string = f.read()
+        records = json.loads(notes_string)
+        for key, value in records.items():
+            record = value
+            for key, value in record.items():
+                print(f'{key} = {value}')
+        
     command = '0'
     return command
 
